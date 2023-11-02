8d937fdae894ea422fc9194adeb5df71c649a8cc
ALoyoVasquez <anitamlv@gmail.com>
2023-11-02 15:11:15 -0400

    espero ultimo cambio

--- test-ministeriales/src/components/test/Brujulas/Character.jsx
+++ test-ministeriales/src/components/test/Brujulas/Character.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import style from "./Character.module.css";
-import imagen from "../../../assets/soyHenry400x400.jpg";
+import tomas from "../../../assets/1tomas.png";
+import martha from "../../../assets/2martha.png";
+import maria from "../../../assets/3maria.png";
+import moises from "../../../assets/4moises.png";
+import pedro from "../../../assets/5pedro.png";
+import jonas from "../../../assets/6jonas.png";
 
 const Character = ({ id, nombre, color, caracteristica, grafico }) => {
   
   const DIR = "./src/assets/"+grafico;
+  // const grafic="";
+
+  // id===1
+  // ? grafic = tomas
+  // : grafic = jonas
+
   // console.log(grafico)
 
   // console.log(DIR);
 
   return (
     <div className={style.layout}>
-      <img src={DIR} alt="grafico" className={style.grafico} />
+      { id===1
+        ? <img src={tomas} alt="grafico" className={style.grafico} />
+        : id===2
+        ? <img src={martha} alt="grafico" className={style.grafico} />
+        : id===3 
+        ? <img src={maria} alt="grafico" className={style.grafico} />
+        : id===4 
+        ? <img src={moises} alt="grafico" className={style.grafico} />
+        : id===5
+        ? <img src={pedro} alt="grafico" className={style.grafico} />
+        : <img src={jonas} alt="grafico" className={style.grafico} />
+    }
       <div className={style.brujula}>
         <div className={style.nombre}>{nombre} </div>
         <div className={style.pc}>
